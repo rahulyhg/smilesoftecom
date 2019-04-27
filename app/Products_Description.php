fa9db975b040fa14aa343359be68752ffb5f7f10
Ashu1992patel <45224160+Ashu1992patel@users.noreply.github.com>
2019-04-27 17:37:42 +0530

    Merge branches 'Ashish' and 'aditya_Ji_bhai_saab' of https://github.com/bijendra7eye/smilesoftecom into Ashish
    # Conflicts:
    #	app/Http/Controllers/WareHouseController.php
    #	resources/views/warehouse/sidebar.blade.php
    #	routes/web.php

--- app/Products_Description.php
+++ app/Products_Description.php
@@ -6,16 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products_Description extends Model
 {
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var bool
-     */
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
     protected $table = 'products_description';
     public $timestamps = false;
     public $primaryKey = 'products_id';
